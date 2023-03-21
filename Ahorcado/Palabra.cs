d073a8f3eda082fa9cc3138efe83c1ff154e0e2f
Dgarrido21 <dgarrido21@alumnos.utalca.cl>
2023-03-21 17:03:12 -0300

    V1.0

---
+++ Ahorcado/Palabra.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colgado
+{
+    class Palabra
+    {
+        //Atributos
+        private string palabra = "";
+        private char[] letra = new char[7];
+        private bool[] verif = new bool[7];
+        private int intentos = 7;
+        //Constructor
+        public Palabra(string palabra)
+        {
+            this.palabra = palabra;
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                letra[i] = palabra[i];
+                verif[i] = false;
+            }
+        }
+        //Funciones
+        public void VerificarLetra(char letra)
+        {
+            if (palabra.Contains(letra))
+            {
+                for (int i = 0; i < palabra.Length; i++)
+                {
+                    if (this.letra[i] == letra)
+                    {
+                        verif[i] = true;
+                    }
+                }
+            }
+            else
+            {
+                intentos--;
+            }
+        }
+        public bool VerificarGanador()
+        {
+            bool ganador = true;
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                if (verif[i] == false)
+                {
+                    ganador = false;
+                }
+            }
+            return ganador;
+        }
+        public bool VerificarPerdedor()
+        {
+            return (intentos <= 0);
+        }
+        public string RecibirPalabra()
+        {
+            string enviar = "";
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                if (verif[i])
+                {
+                    enviar += letra[i];
+                }
+                else
+                {
+                    enviar += '_';
+                }
+                enviar += ' ';
+            }
+            return enviar;
+        }
+        public int GetIntentos()
+        {
+            return intentos;
+        }
+    }
+}
---
+++ Ahorcado/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colgado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Palabra palabra = new Palabra(EscogerPalabra());
+            do
+            {
+                Dibujar(palabra);
+                Turno(palabra);
+            } while (palabra.VerificarGanador() == false && palabra.VerificarPerdedor() == false);
+            if (palabra.VerificarPerdedor())
+            {
+                Console.WriteLine("Has perdido");
+            }
+            if (palabra.VerificarGanador())
+            {
+                Console.WriteLine("Has ganado");
+            }
+        }
+        static string EscogerPalabra()
+        {
+            Random rand = new Random();
+            List<string> palabras = new List<string> { "CERVEZA", "DIEZMAR", "MARACAS", "LEYENDO", "MUTILAR" };
+            return palabras[rand.Next(palabras.Count)];
+        }
+        static char PedirLetra()
+        {
+            char character = 'a';
+            bool errores = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Por favor ingrese una letra valida (sin tilde).");
+                    string characters = Console.ReadLine();
+                    character = Convert.ToChar(characters);
+                    character = char.ToUpper(character);
+                    if (character < 65 || character > 90)
+                    {
+                        throw new Exception("Letra ivalida");
+                    }
+                    errores = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Letra ivalida");
+                }
+            } while (errores);
+            return character;
+        }
+        static void Turno(Palabra palabra)
+        {
+            palabra.VerificarLetra(PedirLetra());
+            Dibujar(palabra);
+        }
+        static void Dibujar(Palabra palabra)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("╔═══════════════╗");
+            Console.WriteLine("║               |");
+            if (palabra.GetIntentos() >= 1)
+            {
+                Console.WriteLine("║               O");
+            }
+            else
+            {
+                Console.WriteLine("║");
+            }
+            if(palabra.GetIntentos() >= 4)
+            {
+                Console.WriteLine("║              /|\\");
+            }
+            else if (palabra.GetIntentos() >= 3)
+            {
+                Console.WriteLine("║              /|");
+            }
+            else if (palabra.GetIntentos() >= 2)
+            {
+                Console.WriteLine("║              /");
+            }
+            else
+            {
+                Console.WriteLine("║");
+            }
+            if (palabra.GetIntentos() >= 5)
+            {
+                Console.WriteLine("║               |");
+            }
+            else
+            {
+                Console.WriteLine("║");
+            }
+            if (palabra.GetIntentos() >= 7)
+            {
+                Console.WriteLine("║              / \\");
+            }
+            else if (palabra.GetIntentos() >= 6)
+            {
+                Console.WriteLine("║              / ");
+            }
+            else
+            {
+                Console.WriteLine("║");
+            }
+            Console.WriteLine("║");
+            Console.WriteLine("║");
+            Console.WriteLine(palabra.RecibirPalabra());
+            Console.WriteLine();
+        }
+    }
+}
